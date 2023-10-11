cbccb64acc83d754a351d12d86f32eb1f0198ec9
stirtion <stirtion@163.com>
2023-10-11 20:01:37 +0800

    init hellolinux

---
+++ src/main/java/com/example/hellolinux/HellolinuxApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellolinux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellolinuxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellolinuxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hellolinux/HellolinuxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellolinux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellolinuxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
